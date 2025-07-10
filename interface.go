f52ba6abe91222144452405a2e723424eb2139c4
Marco <marco23a@hotmail.com>
2025-07-10 14:13:49 -0300

    implement main function and user interaction for note management

---
+++ interface.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func mainSreen() {
+	fmt.Print("1. Add new notes\n")
+	fmt.Print("2. Access your notes\n")
+	fmt.Print("3. Exit")
+}
+
+func AddNotes() {
+	//clear screen between functions
+	fmt.Print("Type your note: ")
+}
+
+func SavedNotes() {
+
+	fmt.Println("Saved notes will be displayed here")
+}
--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var mainOption int
+	mainSreen() // Using the function from interface.go
+	fmt.Print(": ")
+	fmt.Scanln(&mainOption)
+
+	if mainOption == 1 {
+		AddNotes() // Using the function from interface.go
+	} else if mainOption == 2 {
+		SavedNotes() // Using the function from interface.go
+	} else if mainOption == 3 {
+		fmt.Println("Goodbye!")
+	} else {
+		fmt.Println("Invalid option")
+	}
 }
